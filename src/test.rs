8d090f1c899fa462b90140c31473b4063b11b91a
Robert C Jensen <robert.cole.jensen@gmail.com>
2015-04-20 15:15:54 -0400

    updated tests

--- src/test.rs
+++ src/test.rs
@@ -3,14 +3,14 @@ use csgui::{KEY_h, KEY_j, KEY_k, KEY_l, KEY_e, KEY_q};
 
 #[test]
 fn gui_setup() {
-	let my_csgui = CSG::new("test.db");
+	let my_csgui = CSG::new("test.db").unwrap();
 	assert_eq!(my_csgui.active_screen, 0);
 	assert_eq!(my_csgui.screens.len(), 1);
 }
 
 #[test]
 fn main_screen_invalid_movement() {
-	let mut my_csgui = CSG::new("test.db");
+	let mut my_csgui = CSG::new("test.db").unwrap();
 	// Coords should not change when moving outside boundary
 	let coords = my_csgui.get_active_window_coords();
 	let result = my_csgui.dispatch_key(KEY_h);
@@ -21,7 +21,7 @@ fn main_screen_invalid_movement() {
 
 #[test]
 fn main_screen_valid_movement() {
-	let mut my_csgui = CSG::new("test.db");
+	let mut my_csgui = CSG::new("test.db").unwrap();
 	// Coords should change when moving inside boundary
 	let coords = my_csgui.get_active_window_coords();
 	let result = my_csgui.dispatch_key(KEY_j);
@@ -33,7 +33,7 @@ fn main_screen_valid_movement() {
 #[test]
 fn main_screen_edit() {
 	// Edit should activate table dump screen
-	let mut my_csgui = CSG::new("test.db");
+	let mut my_csgui = CSG::new("test.db").unwrap();
 	assert_eq!(my_csgui.active_screen, 0);
 	let result = my_csgui.dispatch_key(KEY_e);
 	assert!(result.is_some());
@@ -43,14 +43,14 @@ fn main_screen_edit() {
 #[test]
 fn main_screen_quit() {
 	// Quit should cease mainloop execution by returning None
-	let mut my_csgui = CSG::new("test.db");
+	let mut my_csgui = CSG::new("test.db").unwrap();
 	let result = my_csgui.dispatch_key(KEY_q);
 	assert!(result.is_none());
 }
 
 // #[test]
 fn table_dump_screen_invalid_movement() {
-	let mut my_csgui = CSG::new("test.db");
+	let mut my_csgui = CSG::new("test.db").unwrap();
 	// Coords should not change, column titles are not selectable
 	let mut result = my_csgui.dispatch_key(KEY_e);
 	assert!(result.is_some());
@@ -63,7 +63,7 @@ fn table_dump_screen_invalid_movement() {
 
 // #[test]
 fn table_dump_screen_valid_movement() {
-	let mut my_csgui = CSG::new("test.db");
+	let mut my_csgui = CSG::new("test.db").unwrap();
 	// Movement to a new column is allowed
 	let mut result = my_csgui.dispatch_key(KEY_e);
 	assert!(result.is_some());
@@ -77,7 +77,7 @@ fn table_dump_screen_valid_movement() {
 #[test]
 fn table_dump_screen_edit() {
 	// XXX: Edit unsupported
-	let mut my_csgui = CSG::new("test.db");
+	let mut my_csgui = CSG::new("test.db").unwrap();
 	assert_eq!(my_csgui.active_screen, 0);
 	let mut result = my_csgui.dispatch_key(KEY_e);
 	assert!(result.is_some());
@@ -90,7 +90,7 @@ fn table_dump_screen_edit() {
 #[test]
 fn table_dump_screen_quit() {
 	// Quit should return to main screen and not exit mainloop
-	let mut my_csgui = CSG::new("test.db");
+	let mut my_csgui = CSG::new("test.db").unwrap();
 	assert_eq!(my_csgui.active_screen, 0);
 	let mut result = my_csgui.dispatch_key(KEY_e);
 	assert!(result.is_some());
